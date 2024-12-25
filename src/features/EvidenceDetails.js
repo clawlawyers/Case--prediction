ed07e4ee678c015076506899d97077654ca1b6f7
rahul9369 <rp25082003@gmail.com>
2024-12-25 16:52:05 +0530

    inital commit

--- src/features/EvidenceDetails.js
+++ src/features/EvidenceDetails.js
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-export const evidenceDetailSlice = createSlice({
-  name: "evidence",
-  initialState: {
-    EvidenceDetail: [],
-  },
+// export const evidenceDetailSlice = createSlice({
+//   name: "evidence",
+//   initialState: {
+//     EvidenceDetail: [],
+//   },
 
-  reducers: {
-    setEvidence(state, action) {
-      state.EvidenceDetail = action.payload.EvidenceDetail;
-    },
-    resetEvidence(state, action) {
-      state.EvidenceDetail = null;
-    },
-  },
-});
+//   reducers: {
+//     setEvidence(state, action) {
+//       state.EvidenceDetail = action.payload.EvidenceDetail;
+//     },
+//     resetEvidence(state, action) {
+//       state.EvidenceDetail = null;
+//     },
+//   },
+// });
 
-export const { setEvidence, resetEvidence } = evidenceDetailSlice.actions;
+// export const { setEvidence, resetEvidence } = evidenceDetailSlice.actions;
 
-export default evidenceDetailSlice.reducer;
+// export default evidenceDetailSlice.reducer;
